d287818f2ae856c59f36d7385930f008d939245b
Nas <nas.shihab92@gmail.com>
2023-07-24 11:52:52 +0600

    Initial commit..

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Github Workflow'),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body: const SafeArea(
+        child: Center(
+          child: Text(
+            'Hello 1',
+            style: TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
+    );
+  }
+}
